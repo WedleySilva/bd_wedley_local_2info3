44b6238bd4fdafd9a5ad040ec712e57baa96bfaa
Aqua <99206531+WedleySilva@users.noreply.github.com>
2022-09-21 11:55:31 -0300

    Update receitascerto.sql

--- wedley/receitascerto.sql
+++ wedley/receitascerto.sql
@@ -12,7 +12,27 @@ cidade_cd_cidade = cd_cidade and
 uf_cd_uf = cd_uf;
 
 select * from cad_usuario;
+select * from pedidos;
 
+select nome, cod_pedido 
+from cad_usuario, pedidos
+where cad_usuario.cpf = pedidos.cad_usuario_cpf;
+
+select nome, cod_pedido, dtped, qtditem, descricao, preco_unit
+from cad_usuario, pedidos, itemped, produto
+where cad_usuario.cpf = pedidos.cad_usuario_cpf and
+pedidos.cod_pedido = itemped.ped_codpedidos and
+itemped.prod_cod_produto = produto.cod_produto;
+
+select nome, cpf, descricao_tipo, ds_logradouro_nome
+from cad_usuario, tipo_usuario, logradouro
+where cad_usuario.tipuser_cd = tipo_usuario.cod_tip_user and
+cad_usuario.log_cd_logradouro = logradouro.cd_logradouro;_cod_produto = produto.cod_produto;
+
+select nome, cpf, descricao_tipo, ds_logradouro_nome
+from cad_usuario, tipo_usuario, logradouro
+where cad_usuario.tipuser_cd = tipo_usuario.cod_tip_user and
+cad_usuario.log_cd_logradouro = logradouro.cd_logradouro;
 select nome, ds_logradouro_nome, ds_bairro_nome, ds_cidade_nome, ds_uf_nome, ds_uf_sigla
 from logradouro, bairros, cidades, uf, cad_usuario
 where 
@@ -20,3 +40,25 @@ bairros_cd_bairro = cd_bairro and
 cidade_cd_cidade = cd_cidade and 
 uf_cd_uf = cd_uf and
 log_cd_logradouro = cd_logradouro;
+
+select * from pedidos;
+
+select nome, cod_pedido 
+from cad_usuario, pedidos
+where cad_usuario.cpf = pedidos.cad_usuario_cpf;
+
+select nome, cod_pedido, dtped, qtditem, descricao, preco_unit
+from cad_usuario, pedidos, itemped, produto
+where cad_usuario.cpf = pedidos.cad_usuario_cpf and
+pedidos.cod_pedido = itemped.ped_codpedidos and
+itemped.prod_cod_produto = produto.cod_produto;
+
+select nome, cpf, descricao_tipo, ds_logradouro_nome
+from cad_usuario, tipo_usuario, logradouro
+where cad_usuario.tipuser_cd = tipo_usuario.cod_tip_user and
+cad_usuario.log_cd_logradouro = logradouro.cd_logradouro;_cod_produto = produto.cod_produto;
+
+select nome, cpf, descricao_tipo, ds_logradouro_nome
+from cad_usuario, tipo_usuario, logradouro
+where cad_usuario.tipuser_cd = tipo_usuario.cod_tip_user and
+cad_usuario.log_cd_logradouro = logradouro.cd_logradouro;
